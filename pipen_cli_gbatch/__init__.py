c37d6f0cdfdbbbfb2aef47443a132d7da5678947
pwwang <pwwang@pwwang.com>
2026-01-05 15:45:45 -0700

    1.1.0

--- pipen_cli_gbatch/__init__.py
+++ pipen_cli_gbatch/__init__.py
@@ -78,7 +78,7 @@ from pipen.cli import AsyncCLIPlugin
 from pipen.scheduler import GbatchScheduler
 from pipen_poplog import LogsPopulator
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __all__ = ("CliGbatchPlugin", "CliGbatchDaemon")
 MOUNTED_CWD = "/mnt/disks/.cwd"
 
