5c88130bfa39f18879760c1de24e75dc0c05fa97
pwwang <pwwang@pwwang.com>
2026-04-22 10:02:50 -0700

    refactor: migrate from poetry to uv

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 This will not be included in the distribution.
-The distribution is managed by poetry
-This file is kept only for
-1. Github to index the dependents
-2. pip install -e .
-Do NOT use this to install this package, unless you handled the dependencies
-by your self:
+The distribution metadata and dependencies are managed by pyproject.toml.
+This file is kept only so Github can index dependents and for legacy editable
+installs.
+Do NOT use this to install this package unless you handle dependencies by
+yourself:
 pip install git+https://...
 """
 from setuptools import setup
