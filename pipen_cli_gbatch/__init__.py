0004134bd720863c78858517502a9e68384f698c
pwwang <pwwang@pwwang.com>
2026-08-10 16:03:47 -0700

    1.2.0a5

--- pipen_cli_gbatch/__init__.py
+++ pipen_cli_gbatch/__init__.py
@@ -84,7 +84,7 @@ from pipen.scheduler import GbatchScheduler
 from pipen_poplog import LogsPopulator
 from pipen_args.parser_ import _pre_parse
 
-__version__ = "1.2.0a4"
+__version__ = "1.2.0a5"
 __all__ = ("CliGbatchPlugin", "CliGbatchDaemon")
 
 
